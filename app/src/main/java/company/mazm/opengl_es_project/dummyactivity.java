42600768569f0b4bb38b1e928d0005ece2810b17
Matteo Zuccolin <Matteo Zuccolin>
2016-04-18 20:18:02 +0200

    blank activity

---
+++ app/src/main/java/company/mazm/opengl_es_project/dummyactivity.java
@@ -0,0 +1,13 @@
+package company.mazm.opengl_es_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class dummyactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dummyactivity);
+    }
+}
